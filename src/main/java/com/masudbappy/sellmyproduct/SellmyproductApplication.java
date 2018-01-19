a7a4d43224474045c6d7b0d2f0c414de43b86313
masudbappy <masud.bappy90@gmail.com>
2018-01-20 00:31:10 +0600

    create project and add dependencies

---
+++ src/main/java/com/masudbappy/sellmyproduct/SellmyproductApplication.java
@@ -0,0 +1,12 @@
+package com.masudbappy.sellmyproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellmyproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellmyproductApplication.class, args);
+	}
+}
